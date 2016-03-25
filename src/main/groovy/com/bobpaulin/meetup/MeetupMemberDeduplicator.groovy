ae8df8831a071618024f85bcd8c315b25659ecaf
Bob Paulin <bob@apache.org>
2016-03-24 20:04:29 -0500

    Added Meetup real name script and instructions.

--- src/main/groovy/com/bobpaulin/meetup/MeetupMemberDeduplicator.groovy
+++ src/main/groovy/com/bobpaulin/meetup/MeetupMemberDeduplicator.groovy
@@ -8,11 +8,20 @@ import static groovyx.net.http.ContentType.JSON
 class MeetupMemberDeduplicator {
     static void main(String... args) {
 
+        if(args.length != 2)
+        {
+            println "Enter Program Arguments <groups> and <key>"
+            println "groups is a comma separated list with no spaces ex 123,4444"
+            println "key is your meetup api key"
+            println "EX"
+            println "MeetupMemberDeduplicator.groovy 123,4444 12423abcadfafqr23"
+            return
+        }
         def http = new HTTPBuilder()
         def resultSet = new HashSet<String>();
         //Enter your group ids
-        def groups = []
-        def key = 'Enter your meetup key here';
+        def groups = "${args[0]}".split(',')
+    def key = "${args[1]}";
         groups.each{
             def url = "https://api.meetup.com/2/members?sign=true&key=${key}&group_id=${it}&photo-host=public&page=200"
             def total = '0'
---
+++ src/main/groovy/com/bobpaulin/meetup/MeetupMemberRealName.groovy
@@ -0,0 +1,96 @@
+package com.bobpaulin.meetup
+
+import groovyx.net.http.HTTPBuilder
+import java.util.concurrent.CountDownLatch
+import static groovyx.net.http.Method.GET
+import static groovyx.net.http.ContentType.JSON
+
+
+class MeetupMemberRealName {
+    static void main(String... args) {
+        
+        if(args.length != 4)
+        {
+            println "This program prints the real name of group members either from thier"
+            println "meetup name or from a custom profile question"
+            println "Enter Program Arguments <groupName> <eventId> <questionNumbr> and <key>"
+            println "groupName is the name of your meetup group (same as your meetup path)"
+            println "eventId is the id of the event your trying to get from meetup"
+            println "questionNumber is the id of the question number that includes the users real name"
+            println "key is your meetup api key"
+            println "EX"
+            println "MeetupMemberRealName.groovy ChicagoJUG 234243 4444444 12423abcadfafqr23"
+            return
+        }
+
+        def http = new HTTPBuilder()
+        def groupName = "${args[0]}"
+        def eventId = "${args[1]}"
+        def idToNameMapping = new HashMap<String>()
+        def attendeeNames = new ArrayList<String>()
+        def requestCompleteLatch = new CountDownLatch(1)
+        def questionNumber = "${args[2]}"
+        def key = "${args[3]}"
+        
+        def url = "https://api.meetup.com/${groupName}/members?&sign=true&key=${key}&photo-host=public&page=200"
+        while(url?.trim()){
+                http.request(url, GET, JSON) { req ->
+                    requestContentType = JSON;
+                    headers.Accept = 'application/json';
+
+                    // response handler for a successful response code:
+                    response.success = { resp, json ->
+                        // Check returned status'
+                        json.each{
+                            it.group_profile.answers.each{
+                                if(it.question_id.equals(Long.parseLong(questionNumber)))
+                                {
+                                    if(it.answer != null && it.answer?.trim())
+                                    {
+                                        idToNameMapping.put(it.id, it.answer);
+                                    }
+                                }
+                            }
+                            
+                        }
+                        def linkHeaderVal = resp.headers['Link'].value;
+                        if(linkHeaderVal.endsWith("rel=\"next\""))
+                        {
+                            url = linkHeaderVal.substring(1,linkHeaderVal.indexOf('>'))
+                        }
+                        else
+                        {
+                            url = ""
+                        }
+                    }
+                }
+        }
+        url = "https://api.meetup.com/${groupName}/events/${eventId}/rsvps?&sign=true&key=${key}&photo-host=public"
+        http.request(url, GET, JSON) { req ->
+            requestContentType = JSON;
+            headers.Accept = 'application/json';
+
+            // response handler for a successful response code:
+            response.success = { resp, json ->
+                // Check returned status'
+                json.each{
+                    def memberId = it.member.id
+                    def fullName = idToNameMapping.get(memberId)
+                    if(fullName)
+                    {
+                        attendeeNames.add(fullName)
+                    }
+                    else
+                    {
+                        attendeeNames.add(it.member.name)
+                    }
+                }
+                attendeeNames.each {  
+                    println it
+                }
+            }
+        }
+        
+        
+    }
+}
